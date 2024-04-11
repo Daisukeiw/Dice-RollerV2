54c1c84fec701c431a6a41bed6a8ef20822fc4d6
Daisukeiw <iwamotov@gmail.com>
2024-04-11 11:01:06 -0300

    .

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,30 +1,36 @@
-package com.example.diceroller
-
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.diceroller.ui.theme.DiceRollerTheme
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.diceroller.ui.theme.DiceRollerTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,17 +67,40 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = result.toString()
-        )
+
+    var input by remember {(mutableStateOf(""))}
+
+    val context = LocalContext.current
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Image(painter = painterResource(imageResource), contentDescription = result.toString())
+
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(stringResource(R.string.roll))
+
+        OutlinedTextField(
+            value = input,
+            onValueChange = {input = it},
+            label = {Text("Digite um número")},
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                result = (1..6).random()
+                val number: Int = input.toInt()
+                val toast: Toast
+                if (result == number){
+                    toast = Toast.makeText(context, "Vitória!", Toast.LENGTH_SHORT)
+                } else {
+                    toast = Toast.makeText(context, "Derrota!", Toast.LENGTH_SHORT)
+                    (context as Activity).finish()
+                }
+                toast.show()
+            }) {
+            Text(stringResource(R.string.roll), fontSize = 24.sp)
         }
+
     }
 }
